548522f0f5480f95eefe762c860fcdcf392dff98
jabad28 <abad.joanne@yahoo.com>
2016-04-29 14:44:30 -0700

    new, create, and show

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -6,6 +6,22 @@ class CreaturesController < ApplicationController
   end
 
   def new
+    @creature = Creature.new
     render :new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature = Creature.new(creature_params)
+
+    if creature.save
+      redirect_to creature_path(creature)
+    end
+  end
+
+  def show
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+    render :show
   end
 end
