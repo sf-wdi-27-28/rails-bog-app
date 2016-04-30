eb7930bfa65e4205f9e7594bf7748f9cdb88eb83
seanvsville <seanvanho@gmail.com>
2016-04-30 14:23:50 -0700

    update success

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -5,4 +5,47 @@ class CreaturesController < ApplicationController
     render :index
   end
 
+  def show
+    @creature = Creature.find(params[:id])
+    render :show
+  end
+
+  def new
+    @creature = Creature.new
+    render :new
+  end
+
+  def create
+    creature = Creature.new(creature_params)
+    if creature.save
+      redirect_to creature_path(creature)
+      # redirect_to creatures_path is equivalent to:
+      # redirect_to "/creatures"
+    end
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @creature = Creature.find(params[:id])
+  end
+
+  def update
+    @creature = Creature.find(params[:id])
+    if @creature.update(creature_params)
+      redirect_to creature_path, notice: 'Creature was udpated successfully.'
+    else
+    render :edit
+    end
+  end
+
+private
+
+def creature_params
+  params.require(:creature).permit(:name, :description)
+end
+
 end
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'creatures#index'
 
-  #METHOD   #PATH                     #CONTROLLER#ACTION    #ALIAS
-  get       '/creatures'    =>        'creatures#index',    as: :creatures
+  #METHOD   #PATH                           #CONTROLLER#ACTION    #ALIAS
+  get       '/creatures'            =>      'creatures#index',    as: :creatures
+  post      '/creatures'            =>      'creatures#create'
+  get       '/creatures/new'        =>      'creatures#new',      as: :new_creature
+  get       '/creatures/:id'        =>      'creatures#show',     as: :creature
+  get       'creatures/:id/edit'    =>      'creatures#edit',     as: :edit_record
+  patch     '/creatures/:id'        =>      'creatures#update'    
 
 end
