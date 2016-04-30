004059ccd5750077a3428340eb58223466857232
srhbnnstr <srhbnnstr@gmail.com>
2016-04-29 18:12:07 -0700

    created edit and update routes/actions

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -35,8 +35,28 @@ class CreaturesController < ApplicationController
     # and save to an instance variable
     @creature = Creature.find_by_id(creature_id)
 
-    # render the show view
+    # render the show view (it has access to instance variable)
     render :show
+  end
+
+  def edit
+    creature_id = params[:id]
+
+    @creature = Creature.find_by_id(creature_id)
+
+    render :edit
+  end
+
+  def update
+    creature_id = params[:id]
+
+    creature = Creature.find_by_id(creature_id)
+
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    creature.update_attributes(creature_params)
+
+    redirect_to creature_path(creature)
   end
 
 end
