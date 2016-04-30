5a881d664c9baa250fcb72b7bd8bd73a62b64a55
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-04-30 16:15:43 -0700

    added routes to create new creature

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -4,5 +4,6 @@
 Rails.application.routes.draw do
   root "creatures#index"
 
-  get"/creatures", to: "creatures#index", as:"creatures"
+  get "/creatures", to: "creatures#index", as:"creatures"
+  get "/creatures/new", to: "creatures#new"
 end
