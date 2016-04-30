580e3a32db9ee8b1abe39ce9125fb60cd8986f64
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-04-30 16:46:08 -0700

    added method to post new creature, render the creature to the page, and to redirect to the index immediately after appending creature to the index page

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -4,7 +4,21 @@ class CreaturesController < ApplicationController
     render :index #render the index view (it has access to instance variable)
   end
   def new
+    @creature = Creature.new
     render :new
   end
+  def create
+    # whitelist params and save them to a variable
+    creature_params = params.require(:creature).permit(:name, :description)
 
+    # create a new creature from `creature_params`
+    creature = Creature.new(creature_params)
+
+    # if creature saves, redirect to route that displays all creatures
+    if creature.save
+      redirect_to creatures_path
+      # redirect_to creatures_path is equivalent to:
+      # redirect_to "/creatures"
+    end
+  end
 end
