223d7da1bd449e2d797b09b945f124608efd2505
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-04-30 14:39:39 -0700

    update in controller

--- bog_app/bog_app/app/controllers/creatures_controller.rb
+++ bog_app/bog_app/app/controllers/creatures_controller.rb
@@ -38,6 +38,26 @@ class CreaturesController < ApplicationController
     end
   end
 
+  def update
+    # get the creature id from the url params
+    creature_id = params[:id]
+    
+    # use `creature_id` to find the creature in the database
+         # and save it to an instance variable
+    @creature = Creature.find_by_id(creature_id)
+
+    # whitelist params and save them to a variable
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    #update the creature
+    creature.update_attributes(creature_params)
+
+    #redirect to show page for the updated creature
+    redirect_to creature_path(creature)
+    # redirect_to creature_path(creature) is equivalent to:
+     # redirect_to "/creatures/#{creature.id}"
+  end
+
   def edit
 
     creature_id = params[:id]
@@ -45,6 +65,6 @@ class CreaturesController < ApplicationController
     @creature = Creature.find_by_id(creature_id)
 
     render :edit
-    
+
   end
 end
