24a49cb8e9795112be396758267a6ac43aa8797a
scoobaroo <scoobaroo415@gmail.com>
2016-04-29 18:09:26 -0700

    working delete buton

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -41,4 +41,13 @@ class CreaturesController < ApplicationController
     @creature.update(creature_params)
     redirect_to creature_path(@creature)
   end
+  def destroy
+    # get the creature id from the url params
+    creature_id = params[:id]
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    @creature = Creature.find_by_id(creature_id)
+    @creature.destroy
+    redirect_to creatures_path
+  end
 end
