72843f863dea992adcbb5d3a5227736744e708fe
scoobaroo <scoobaroo415@gmail.com>
2016-04-29 17:45:15 -0700

    created delete and edit update functions

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -7,13 +7,20 @@ class CreaturesController < ApplicationController
     @creature = Creature.new
     render :new
   end
+  def edit
+    # get the creature id from the url params
+    creature_id = params[:id]
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    @taco = Creature.find_by_id(creature_id)
+    # render the edit view (it has access to instance variable)
+    render :edit
+  end
   def create
     # whitelist params and save them to a variable
     creature_params = params.require(:creature).permit(:name, :description)
-
     # create a new creature from `creature_params`
     creature = Creature.new(creature_params)
-
     # if creature saves, redirect to route that displays all creatures
     if creature.save
       redirect_to creatures_path
@@ -24,12 +31,39 @@ class CreaturesController < ApplicationController
   def show
     # get the creature id from the url params
     creature_id = params[:id]
-
     # use `creature_id` to find the creature in the database
     # and save it to an instance variable
     @creature = Creature.find_by_id(creature_id)
-
     # render the show view (it has access to instance variable)
     render :show
+  def destroy
+    # get the creature id from the url params
+    creature_id = params[:id]
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    creature = Creature.find_by_id(creature_id)
+    # destroy the creature
+    creature.destroy
+    # redirect to creatures index
+    redirect_to creatures_path
+    # redirect_to creatures_path is equivalent to:
+    # redirect_to "/creatures"
+  end
+  # update a creature in the database
+  def update
+    # get the creature id from the url params
+    creature_id = params[:id]
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    creature = Creature.find_by_id(creature_id)
+    # whitelist params and save them to a variable
+    creature_params = params.require(:creature).permit(:name, :description)
+    # update the creature
+    creature.update_attributes(creature_params)
+    # redirect to show page for the updated creature
+    redirect_to creature_path(creature)
+    # redirect_to creature_path(creature) is equivalent to:
+    # redirect_to "/creatures/#{creature.id}"
+  end
   end
 end
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'creatures#index'
-
-  get '/creatures', to: "creatures#index", as: "creatures"
-  get "/creatures/new", to: "creatures#new", as: "new_creature"
-  post 'creatures' , to: "creatures#create"
-  get "creatures/:id", to:"creatures#show", as: "show_creature"
+  get '/creatures', to:"creatures#index", as: "creatures"
+  get "/creatures/new", to:"creatures#new", as: "new_creature"
+  post '/creatures' , to:"creatures#create"
+  get "/creatures/:id", to:"creatures#show", as: "show_creature"
+  get "/creatures/:id/edit", to:"creatures#edit", as: "edit_creature"
+  delete "/creatures/:id", to: "creatures#destroy"
+  patch "/creatures/:id", to: "creatures#update"
 end
