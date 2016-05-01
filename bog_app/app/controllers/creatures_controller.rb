cdcd46548d3f52e289f46e97150a2f8b76aa6470
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-04-30 19:34:30 -0700

    added route view and controller for update

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -30,4 +30,13 @@ class CreaturesController < ApplicationController
     @creature = Creature.find_by_id(creature_id)
     render :edit
   end
+
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
 end
