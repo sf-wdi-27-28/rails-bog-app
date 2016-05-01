76e58cee515d4a5f8dd5b758376723500c996823
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-01 08:24:52 -0700

    all routes created

--- bog_app/bog_app/config/routes.rb
+++ bog_app/bog_app/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
 root "creatures#index"
+
+get "/creatures", to:"creatures#index", as:"creatures"
+get "/creatures/new", to:"creatures#new", as:"new_creatures"
+post "/creatures", to:"creatures#create"
+get  "/creatures/:id", to:"creature#show", as: "creature"
+get  "/creatures/:id/edit", to:"creature#edit"
+patch "/creatures/:id", to:"creature#update"
+delete "/creatures/:id", to:"creature#destroy"
+
 end
