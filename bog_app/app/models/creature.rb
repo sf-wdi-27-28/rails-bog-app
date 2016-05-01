3301fe2ebc6a7fb953440117b9c9727f89ee011c
eamoses <e.a.moses@gmail.com>
2016-04-30 20:10:45 -0700

    added validation you have to fill both forms but the error message isnt working

--- bog_app/app/models/creature.rb
+++ bog_app/app/models/creature.rb
@@ -1,2 +1,11 @@
 class Creature < ActiveRecord::Base
+  validates :name, :description, presence: true
 end
+
+creature = Creature.new
+creature.valid?
+creature.errors.messages
+
+creature = Creature.new(name: "Luke Skywalker", description: "Jedi")
+creature.valid?
+creature.errors.messages
