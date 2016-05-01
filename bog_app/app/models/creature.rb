539c78f6123c7b53edb85d223dc01274a47331e0
eamoses <e.a.moses@gmail.com>
2016-04-30 21:02:16 -0700

    working on bonus but the plane is about to land

--- bog_app/app/models/creature.rb
+++ bog_app/app/models/creature.rb
@@ -1,11 +1,2 @@
 class Creature < ActiveRecord::Base
-  validates :name, :description, presence: true
 end
-
-creature = Creature.new
-creature.valid?
-creature.errors.messages
-
-creature = Creature.new(name: "Luke Skywalker", description: "Jedi")
-creature.valid?
-creature.errors.messages
