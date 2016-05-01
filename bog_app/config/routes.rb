1ac8f098273c393df5dc620ff0ab4403c46ca9ee
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-04-30 17:24:43 -0700

    added route for editing creatures

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   root "creatures#index"
 
   get "/creatures", to: "creatures#index", as:"creatures"
-  get "/creatures/new", to: "creatures#new"
+  get "/creatures/new", to: "creatures#new", as: "new_creature"
   post "/creatures", to: "creatures#create"
   get "/creatures/:id", to: "creatures#show", as: "creature"
+  post "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
 end
