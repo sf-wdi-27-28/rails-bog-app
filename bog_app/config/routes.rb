ffba4930f1f9371e3ccf78ee437d3ccd236d74e1
cloverharvest <anna@schwabdev.com>
2016-05-01 15:47:39 -0700

    Built index routes and controllers, creature model, and installedd hirb

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "creatures#index"
 
   get "creatures", to: "creatures#index", as: "creatures"
+  # get "creature/:id", to: "creatures#show", as: "show_creature"
 end
