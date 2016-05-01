bf0fd91decd0defb75754f2c77d75cda039c3372
andreaainara <andrea.ainara10@gmail.com>
2016-04-30 17:15:01 -0700

    update, save, show

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -22,14 +22,14 @@ class CreaturesController < ApplicationController
 
     # if creature saves, redirect to path that shows all creatures ("/creatures")
     if creature.save
-      redirect_to creatures_path
+      redirect_to creatures_path(creature)
     end
   end
 
   # showing a  single creature
   def show
     # get the creature from url params
-    creature_id - params[:id]
+    creature_id = params[:id]
 
     # use 'creature_id' to find the creature in the database
     # save it to an instance variable
@@ -37,4 +37,38 @@ class CreaturesController < ApplicationController
     # render the show view
     render :show
   end
+
+  def edit
+    # using params
+    creature_id = params[:id]
+
+    # use "creature_id"
+    # "save to instance variable"
+    @creature = Creature.find_by_id(creature_id)
+
+    # render edit view
+    render :edit
+  end
+
+  # update a creature_id
+  def update
+    # get the creature id from the url params
+    creature_id = params[:id]
+
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    creature = Creature.find_by_id(creature_id)
+
+    # whitelist params and save them to a variable
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    # update the creature
+    creature.update_attributes(creature_params)
+
+    # redirect to show page for the updated creature
+    redirect_to creature_path(creature)
+    # redirect_to creature_path(creature) is equivalent to:
+    # redirect_to "/creatures/#{creature.id}"
+  end
+
 end
