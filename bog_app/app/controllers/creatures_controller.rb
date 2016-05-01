fce7a0795cb72ba3d0cb8732a79644c649b12356
YOUR_GITHUB_USERNAME <jacquelynfambrough@gmail.com>
2016-04-30 17:43:45 -0700

    added method to delete a creature

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -61,6 +61,14 @@ class CreaturesController < ApplicationController
     redirect_to creature_path(creature)
     #redirect_to creature_path(creature) is equivalent to:
     #redirect_to "/creatures/#{creature.id}"
+  end
+  def destroy
+    creature_id = params[:id]
 
+    creature = Creature.find_by_id(creature_id)
+
+    creature.destroy
+
+    redirect_to creatures_path
   end
 end
