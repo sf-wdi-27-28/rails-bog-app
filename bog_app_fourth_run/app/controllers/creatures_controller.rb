db1987f7e3f8beca684059c57beaaacfcff07c4b
PJC-1 <jumpzine@gmail.com>
2016-05-01 20:54:17 -0700

    edit view is up with links

--- bog_app_fourth_run/app/controllers/creatures_controller.rb
+++ bog_app_fourth_run/app/controllers/creatures_controller.rb
@@ -7,8 +7,14 @@ class CreaturesController < ApplicationController
     creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
     if creature.save
-      redirect_to creatures_path
+      redirect_to creature_path(creature)
     end
+  end
+
+  def edit
+    creature_id = params[:id]
+    @creature =  Creature.find_by_id(creature_id)
+    render :edit
   end
 
   def show
