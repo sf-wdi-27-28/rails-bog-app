9ebc8685408e08c461f76295430de9f2a9ef5667
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-02 07:08:23 -0700

    show and create finished

--- bog_app/bog_app/app/controllers/creatures_controller.rb
+++ bog_app/bog_app/app/controllers/creatures_controller.rb
@@ -11,4 +11,26 @@ class CreaturesController < ApplicationController
 
     render :new
   end
+
+  def show
+
+    creature_id = params[:id]
+     # you use the creature_id to get the creature params and id
+     #from the database then save it to an instance variable
+    @creature =Creature.find_by_id(creature_id)
+
+    render :show
+  end
+
+  def created
+
+    creature_params = params.require(.creature).permit(:name, :description)
+
+    creature = Creature.new(creature_params)
+
+    if creature.Save
+      redirect_to creature_path(creature)
+      # this redirects_to "/creatures"
+    end
+  end
 end
