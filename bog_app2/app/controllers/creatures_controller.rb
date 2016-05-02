8a43a4ab8e71836261d56fec93ca3377bee7957e
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-01 22:47:15 -0700

    added show route and action view

--- bog_app2/app/controllers/creatures_controller.rb
+++ bog_app2/app/controllers/creatures_controller.rb
@@ -10,8 +10,12 @@ class CreaturesController < ApplicationController
   def create
     @creature = Creature.new(creature_params)
     if @creature.save
-      redirect_to creatures_path
+      redirect_to creature_path(@creature)
     end
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
   end
 
   private
