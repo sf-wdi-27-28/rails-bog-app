6dad66d53e496e4132e8ef5b232dbdbbf38f3604
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-05-01 22:48:13 -0700

    controllers and models created

---
+++ bog_app/app/controllers/creatures_controller.rb
@@ -0,0 +1,23 @@
+class CreaturesController < ApplicationController
+  def index
+    @creatures = Creature.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'creatures/index'
+
+  get 'creatures/new'
+
+  get 'creatures/create'
+
+  get 'creatures/show'
+
+  get 'creatures/edit'
+
+  get 'creatures/update'
+
+  get 'creatures/destroy'
+
   root "creatures#index"
 
   get "/creatures", to: "creatures#index", as: "creatures"
