de74cdfd88367d4739c37db0d2051e8f69a33e84
cloverharvest <anna@schwabdev.com>
2016-05-02 08:14:28 -0700

    Adds more validation details, changes to navbar default

--- bog_app/app/models/creature.rb
+++ bog_app/app/models/creature.rb
@@ -1,6 +1,6 @@
 class Creature < ActiveRecord::Base
-  validates :name, presence: true
-  validates :description, presence: true
+  validates :name, presence: true, length: { minimum: 1, maximum: 30, too_short: "must have at least %{count} character(s)", too_long: "must have at most %{count} characters" }
+  validates :description, presence: true, length: {maximum: 255, too_long: "%{count} characters is the maximum allowed" }
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "150x150#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
