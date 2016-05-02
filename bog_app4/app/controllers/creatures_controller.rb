d3f167bb48d5eccb9a336148c5667a93e7bee5b7
Herman Chan <hermchan89@gmail.com>
2016-05-02 00:29:28 -0700

    revised create controller to route to specific creature when created, added show.html, show route

--- bog_app4/app/controllers/creatures_controller.rb
+++ bog_app4/app/controllers/creatures_controller.rb
@@ -10,4 +10,24 @@ class CreaturesController < ApplicationController
     render :new
   end
 
+  def create
+
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    creature = Creature.new(creature_params)
+
+    if creature.save
+      redirect_to creature_path(creature)
+    end
+  end
+
+  def show
+
+    creature_id = params[:id]
+
+    @creature = Creature.find_by_id(creature_id)
+
+    render :show
+  end
+
 end
