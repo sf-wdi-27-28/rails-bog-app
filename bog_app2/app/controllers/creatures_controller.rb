fb53c38b57440e096ec6b20a5496d84cea31dacc
AdrianaVillagran <anvillagran1@gmail.com>
2016-05-01 22:39:49 -0700

    added create route and action view

--- bog_app2/app/controllers/creatures_controller.rb
+++ bog_app2/app/controllers/creatures_controller.rb
@@ -6,5 +6,18 @@ class CreaturesController < ApplicationController
   def new
     @creature = Creature.new
   end
-  
+
+  def create
+    @creature = Creature.new(creature_params)
+    if @creature.save
+      redirect_to creatures_path
+    end
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
+
 end
--- bog_app2/config/routes.rb
+++ bog_app2/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get "/creatures", to: "creatures#index", as: "creatures"
   get "/creatures/new", to: "creatures#new", as: "new_creature"
+  post "/creatures", to: "creatures#create"
 
 end
