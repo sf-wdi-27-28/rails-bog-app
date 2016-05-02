b62afece52d54e19cdc11e002556586481abdbf1
Hila Vasiler <hila.marmel@gmail.com>
2016-05-02 08:04:10 -0700

    added create functionality

--- bog_app/bog_app/bog_app/bog_app/app/controllers/creatures_controller.rb
+++ bog_app/bog_app/bog_app/bog_app/app/controllers/creatures_controller.rb
@@ -5,9 +5,23 @@ class CreaturesController < ApplicationController
     render :index
   end
 
-def new
-  @creature = Creature.new
-  render :new
-end
+  def new
+    @creature = Creature.new
+    render :new
+  end
+
+  def create
+    @creature = Creature.create(creature_params)
+    redirect_to creatures_path
+
+  end
+
+
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
 
 end
