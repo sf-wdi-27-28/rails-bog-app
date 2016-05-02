becd290b31494707ce7267fd7b7d3578ed321520
jtam11 <justinktam@gmail.com>
2016-05-01 17:26:47 -0700

    Created new/create route/action/view

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -3,5 +3,20 @@ class CreaturesController < ApplicationController
   def index
     @creatures = Creature.all
   end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    Creature.create(creature_params)
+    redirect_to creatures_path
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "creatures#index"
 
   get "/creatures" => "creatures#index", as: "creatures"
+  get "/creatures/new" => "creatures#new", as: "new_creature"
+  post "/creatures" => "creatures#create"
   
 end
