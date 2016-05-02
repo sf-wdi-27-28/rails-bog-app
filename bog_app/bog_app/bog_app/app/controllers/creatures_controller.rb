28b51e25b30a09d02e1032e8bd2d7c9eae195d20
hectorcoronado <coronado.hector@gmail.com>
2016-05-01 23:23:59 -0700

    28m 32s create

--- bog_app/bog_app/bog_app/app/controllers/creatures_controller.rb
+++ bog_app/bog_app/bog_app/app/controllers/creatures_controller.rb
@@ -3,5 +3,16 @@ class CreaturesController < ApplicationController
   def index
     @creatures = Creature.all
   end
-  
+
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+    Creature.create(creature_params)
+    redirect_to creatures_path
+  end
+
 end
