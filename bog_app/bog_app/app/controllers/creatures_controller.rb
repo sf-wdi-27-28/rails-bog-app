10c9058cacbda67ca8995dccd618c5eabc2cfbe6
relative-rene <rene.arellano415@gmail.com>
2016-05-02 08:19:15 -0700

    resources and before_aciton refactor

--- bog_app/bog_app/app/controllers/creatures_controller.rb
+++ bog_app/bog_app/app/controllers/creatures_controller.rb
@@ -1,5 +1,6 @@
 class CreaturesController < ApplicationController
-
+  before_action :get_id, only: [:update, :delete, :edit, :show]
+  
   def index
     @creatures = Creature.all
 
@@ -12,30 +13,34 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
-    creature = Creature.new(creature_params)
-
     if creature.save
       redirect_to creature_path(creature)
     end
   end
 
   def show
-    creature_id = params[:id]
-    @creature = Creature.find_by_id(creature_id)
-
     render :show
   end
 
   def edit
+  render :edit
+  end
+
+  def update
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature.update_attributes(creature_params)
+    redirect_to creature(creature)
+  end
+
+  def destroy
+    creature.destroy
+    redirect_to creatures_path
+  end
+
+  private
+
+  def grab_id
     creature_id = params[:id]
     @creature = Creature.find_by_id(creature_id)
-
-    render :edit
   end
-
-  def delete
-  end
-
-
 end
--- bog_app/bog_app/config/routes.rb
+++ bog_app/bog_app/config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
+resources :creatures
   root "creatures#index"
-
-  get "/creatures", to: 'creatures#index', as: "creatures"
-  get "/creatures/new", to: "creatures#new", as: "new_creature"
-  post "/creatures", to: "creatures#create"
-  get "/creatures/:id", to: "creatures#show", as: "creature"
-  get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
-  delete
-
 end
