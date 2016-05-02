3292fa21c25f1be663935e04782704efd21f1b67
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-02 07:33:00 -0700

    update created and comments added

--- bog_app/bog_app/app/controllers/creatures_controller.rb
+++ bog_app/bog_app/app/controllers/creatures_controller.rb
@@ -17,20 +17,49 @@ class CreaturesController < ApplicationController
     creature_id = params[:id]
      # you use the creature_id to get the creature params and id
      #from the database then save it to an instance variable
-    @creature =Creature.find_by_id(creature_id)
+    @creature = Creature.find_by_id(creature_id)
 
     render :show
   end
 
   def created
 
-    creature_params = params.require(.creature).permit(:name, :description)
+    creature_params = params.require(:creature).permit(:name, :description)
 
-    creature = Creature.new(creature_params)
+    @creature = Creature.new(creature_params)
 
     if creature.Save
       redirect_to creature_path(creature)
       # this redirects_to "/creatures"
+    end
+
+    def edit
+
+      creature_id = params[:id]
+
+      @creature = Creature.find_by_id(creature_id)
+
+      # renders the dit view (this has access to the instance variable)
+      render :edit
+
+
+    end
+
+    def update
+
+      creature_id = params[:id]
+
+      @creature = Creature.find_by_id(creature_id)
+
+      creature_params = params.require(:creature).permit(:name, :description)
+
+      # update the creature Updates a single attribute and saves the record without going through the normal validation procedure.
+      creature.update_attributes(creature_params)
+
+      redirect_to creature_path(creature)
+
+
+
     end
   end
 end
--- bog_app/bog_app/config/routes.rb
+++ bog_app/bog_app/config/routes.rb
@@ -5,7 +5,7 @@ get "/creatures", to:"creatures#index", as:"creatures"
 get "/creatures/new", to:"creatures#new", as:"new_creatures"
 post "/creatures", to:"creatures#create"
 get  "/creatures/:id", to:"creature#show", as: "creature"
-get  "/creatures/:id/edit", to:"creature#edit"
+get  "/creatures/:id/edit", to:"creature#edit", as: "edit_creature"
 patch "/creatures/:id", to:"creature#update"
 delete "/creatures/:id", to:"creature#destroy"
 
