10d253a6256a1f147bb07e189ffcfe174d28a1bc
mradambeck <mr.adambeck@gmail.com>
2016-05-01 22:19:33 -0700

    more stuff being done.

---
+++ bog_app/app/controllers/creatures_controller.rb
@@ -0,0 +1,31 @@
+class CreaturesController < ApplicationController
+
+  def index
+    @creatures = Creature.all
+    render :index
+  end
+
+  def new
+    @creature = Creature.new
+    render :new
+  end
+
+  def create
+    creature = Creature.new(creature_params)
+    if creature.save
+      redirect_to creature_path(creature)
+    end
+  end
+
+  def show
+    @creature = Creature.find_by_id(params[:id])
+    render :show
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
+  end
+
+end
