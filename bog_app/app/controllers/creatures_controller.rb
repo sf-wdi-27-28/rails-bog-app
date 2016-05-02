3b287b618357e41cae1a1390c5a1f1c1831651ac
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-05-01 21:34:30 -0700

    created 'create'route/view and updated 'new'

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -4,5 +4,14 @@ class CreaturesController < ApplicationController
   end
 
   def new
+    @creature = Creature.new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature = Creature.new(creature_params)
+    if creature.save
+      redirect_to creatures_path
+    end
   end
 end
