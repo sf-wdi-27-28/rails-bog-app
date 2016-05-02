b29243a3a8629e51ac5daca81fbe574adc9c6fe9
Herman Chan <hermchan89@gmail.com>
2016-05-02 00:01:10 -0700

    new form to create creature, added button to direct to form page, button to go back to index, new route, controller

--- bog_app4/app/controllers/creatures_controller.rb
+++ bog_app4/app/controllers/creatures_controller.rb
@@ -2,8 +2,12 @@ class CreaturesController < ApplicationController
 
   def index
     @creatures = Creature.all
-
     render :index
+  end
+
+  def new
+    @creature = Creature.new
+    render :new
   end
 
 end
--- bog_app4/config/routes.rb
+++ bog_app4/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "creatures#index"
 
   get "/creatures", to: "creatures#index"
+  get "/creatures/new", to: "creatures#new", as: "new_creature"
 
 end
