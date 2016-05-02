583d62e11b5aeaab021e3552daf7b6d9e1e4aa84
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-05-01 21:38:20 -0700

    got 'show' all done

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -11,7 +11,12 @@ class CreaturesController < ApplicationController
     creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
     if creature.save
-      redirect_to creatures_path
+      redirect_to creatures_path(creature)
     end
+  end
+
+  def show
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
   end
 end
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "/creatures", to: "creatures#index", as: "creatures"
   get "/creatures/new", to: "creatures#new"
   post "/creatures", to: "creatures#create"
+  get "/creatures/:id", to: "creatures#show", as: "creature"
 
 end
