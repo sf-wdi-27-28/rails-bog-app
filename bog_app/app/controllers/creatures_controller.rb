bd3ce27c913467b674228981368d10c592caf0c3
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-05-02 00:07:11 -0700

    cruising along

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -1,23 +1,34 @@
 class CreaturesController < ApplicationController
   def index
-    # @creatures = Creature.all
+    @creatures = Creature.all
   end
 
   def new
+    render :new
   end
 
   def create
+    @creature = Creature.create(creature_params)
+    redirect_to creature_path
   end
 
   def show
+    @creature = Creature.find_by_id params[:id]
   end
 
   def edit
+    @creature = Creature.find_by_id params[:id]
   end
 
   def update
   end
 
   def destroy
+  end
+
+private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
   end
 end
