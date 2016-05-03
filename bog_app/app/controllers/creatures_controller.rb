ee6e3e5440e5be88c35e07c6df64c189b8eaffa3
smicl11 <bryan.smith094@gmail.com>
2016-05-02 17:56:07 -0700

    part 2

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -3,4 +3,35 @@ class CreaturesController < ApplicationController
     @creatures = Creature.all
     render :index
   end
+
+  def new
+    @creature = Creature.new
+    render :new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature = Creature.new(creature_params)
+
+    if creature.save
+      redirect_to creatures_path
+    end
+  end
+
+  def show
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+    render :show
+  end
+
+  def create
+   creature_params = params.require(:creature).permit(:name, :description)
+   creature = Creature.new(creature_params)
+
+   if creature.save
+     redirect_to creature_path(creature)
+   end
+  end
+
+
 end
